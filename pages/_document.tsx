d5ffcfbd09ef37a00fd682f3d9cbe4e279592c9d
mawburn <mawburn7@gmail.com>
2022-08-13 16:55:49 -0400

    add document.tsx

---
+++ pages/_document.tsx
@@ -0,0 +1,25 @@
+import Document, { Head, Html, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <meta name="description" content="Stock images for commercial use, free and paid." />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Fira+Mono&family=Inter:wght@300;400;700&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
